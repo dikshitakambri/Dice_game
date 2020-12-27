5bd9cc51ddc88f9afe80830280beaa2248608303
dikshitakambri <dikshitakambri18@gmail.com>
2020-12-27 13:56:42 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>Dice Game 🎲</h1>
+
+<p>In this game, there are two dice. These two dice are rolled simultaneously by the players. The player who got a greater number of dice will win.🏆 </p>
+<p>Just click on start button and it will start game</p>
+
+<h4>Languages Used</h4>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Javascript</li>
+</ul>
+
+<h4>Libraries Used</h4>
+<ul>
+    <li>Bootstrap(Frontend library)</li>
+</ul>
+
+![alt text](Images/game.png)
--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-var randomnumber1 = Math.floor(Math.random() * 6) +1;
+function gamestart(){
+    var randomnumber1 = Math.floor(Math.random() * 6) +1;
 
-var imagesrc1 = "Images/" + randomnumber1 + ".png";
+    var imagesrc1 = "Images/" + randomnumber1 + ".jpeg";
 
-var x = document.querySelectorAll("img")[0];
+    var x = document.querySelectorAll("img")[0];
 
-x.setAttribute("src", imagesrc1);
+    x.setAttribute("src", imagesrc1);
 
-var y = document.querySelectorAll("img")[1]; 
+    var y = document.querySelectorAll("img")[1]; 
 
-var randomnumber2 = Math.floor(Math.random() * 6) +1;
+    var randomnumber2 = Math.floor(Math.random() * 6) +1;
 
-var imagesrc2 = "Images/" + randomnumber2 + ".png";
+    var imagesrc2 = "Images/" + randomnumber2 + ".jpeg";
 
-y.setAttribute("src",imagesrc2);
+    y.setAttribute("src",imagesrc2);
 
-if(randomnumber1 > randomnumber2){
-    document.querySelector("h1").innerHTML= "Player 1 Won the match🚩";
-}
-else if(randomnumber2 > randomnumber1){
-    document.querySelector("h1").innerHTML= "Player 2 Won the match🚩";
-}
-else {
-    document.querySelector("h1").innerHTML= "It's a tie";
+    if(randomnumber1 > randomnumber2){
+        document.querySelector("h3").innerHTML= "Player 1 Won the match🚩";
+    }
+    else if(randomnumber2 > randomnumber1){
+        document.querySelector("h3").innerHTML= "Player 2 Won the match🚩";
+    }
+    else {
+        document.querySelector("h3").innerHTML= "It's a tie";
+    }
+
 }
